fda8b8b37caa17e4958b304f43b6a576fd2afcf4
Daniela Tati <danielataty@icloud.com>
2025-01-07 19:16:30 +0100

    Seção de serviços terminada

--- js/script.js
+++ js/script.js
@@ -13,13 +13,52 @@ window.addEventListener("scroll", function () {
     }
 });
 
-/*Elemento que se move */
 
-var star = document.getElementById("star")
+var icone = document.getElementById("icone")
+var card = document.getElementById("card")
 
-window.addEventListener("mousemove", (e) =>{
-    const x = e.clientX; // Pega a posição X do mouse
-    const y = e.clientY;
+/*Quando o mause passar por dentro do card a imagem troca e vice versa 1ª */
+// Primeiro card
+var card1 = document.getElementById("card1");
+var icone1 = document.getElementById("icone1");
 
-    star.style.transform = `translate(${x}px, ${y}px)`;
-})
+card1.addEventListener("mouseenter", function () {
+    icone1.src = "img/Icones/websites1.png";
+});
+card1.addEventListener("mouseleave", function () {
+    icone1.src = "img/Icones/websites3.png";
+});
+
+// Segundo card
+var card2 = document.getElementById("card2");
+var icone2 = document.getElementById("icone2");
+
+card2.addEventListener("mouseenter", function () {
+    icone2.src = "img/Icones/design1.png";
+});
+card2.addEventListener("mouseleave", function () {
+    icone2.src = "img/Icones/design3.png";
+});
+
+// Terceiro card
+var card3 = document.getElementById("card3");
+var icone3 = document.getElementById("icone3");
+
+card3.addEventListener("mouseenter", function () {
+    icone3.src = "img/Icones/redes1.png";
+});
+card3.addEventListener("mouseleave", function () {
+    icone3.src = "img/Icones/redes3.png";
+});
+
+// Quarto card
+var card4 = document.getElementById("card4");
+var icone4 = document.getElementById("icone4");
+
+card4.addEventListener("mouseenter", function () {
+    icone4.src = "img/Icones/mark1.png";
+});
+card4.addEventListener("mouseleave", function () {
+    icone4.src = "img/Icones/mark3.png";
+});
+
